592e7c6443e9a858c2b8d8c09afdc375191c3abb
Samarth-HTML123 <saritareddyjillela@gmail.com>
2026-08-15 07:33:29 -0300

    Add files via upload

---
+++ reverse string.py
@@ -0,0 +1,8 @@
+string = input("Please ener your own String  :  ")
+
+string2 = ('Samarth')
+
+for i in string:
+    string2 = i + string2
+    print("\nThe Original String =", string)
+    print("The Reverse String = ", string2)
